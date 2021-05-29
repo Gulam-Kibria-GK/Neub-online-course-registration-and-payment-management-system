b264ba5bf508c120d586d4f5f5d9b9e748d20756
Gulam_Kibria <gkpalash101@gmail.com>
2021-05-29 21:00:01 +0600

    1st commit

---
+++ Academic Programs.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Acadenic Programs
+		</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	
+	<body>
+		<div class="full_height" >
+				
+				<div class="head">
+				
+					<div class="head1">
+
+						<div class="head1_pic">
+							<img src="logo.jpg" width="98px" height="78px">
+						</div>
+						<div class="head1_ex1">
+						</div>
+						<div class="head1_name">
+							<h3 style="text-align:left">North East University Bangladesh</h3>
+						</div>
+						<div class="head1_sign_log">
+								<!-- <div style="width:100%;height:18px;float:left;background:#0D3336">
+									
+								</div> -->
+								<div class="head1_signUp">
+									<h4 ><a href="reg.php" style="text-decoration:none">Sign Up</a></h4>
+								</div>
+								<div class="head1_signIn">
+									<h4 ><a href="Login.php">Sign In</a></h4>
+								</div>
+								<div class="head1_sign_log_ex1">
+									
+								</div>								
+								
+								<!-- <div style="width:100%;height:20px;float:left;background:#0D3336">
+	
+								</div> -->
+						</div>
+					</div>
+					
+					<hr style="width:100%; height:1px;float:left;margin:0%;background:#162199">
+					
+					<div class="head2" id="navbar">
+					
+						<!-- <div style="width:10%; height:65px;border:0px solid white;float:left;background:#0D3336;">
+						
+						</div> -->
+						<div class="head2_manu1">
+						<h4 ><a href="home.php">Home</a></h4>
+						</div>
+						<div class="head2_manu1">
+						<h4 ><a href="Academic Programs.php">About Us</a></h4>
+						</div>
+						<div class="head2_manu1">
+							<h4 ><a href="tution_fee.php">Admission</a></h4>
+						</div>
+						<div class="head2_manu2">
+							<h4 >Faculty Members</h4>
+						</div>
+						<div class="head2_manu2">
+							<h4 ><a href="Student Portal.php" style="text-decoration:none">Student Portal</a></h4>
+						</div>
+						<div class="head2_manu1">
+							<h4 >Office</h4>
+						</div>
+						<div  class="head2_manu2">
+							<h4 >Others</h4>
+						</div>
+						<div  style="width:15.35%;float:left;	background:#0D3336;height:65px;min-width:123px">
+							<h4 ></h4>
+						</div>
+					</div>
+				</div>
+				
+				<hr style="width:100%; height:2px;float:left;margin:0%;background:#162199">
+				
+				<div style="width:70%; height:auto;min-width:320px;color:white;float:left">
+					<div style="width:1%;height:700px;background:white;float:left">
+							
+					</div>
+					
+					<div style="width:99%; height:auto;min-width:320px;background:#D6E0E0;color:black;float:left">
+			
+					<div style="height:auto;min-width:320px;">
+						<h1 style="font-size:2vw">UnderGraduate Program:</h1>
+
+						<table style="height: 217px;" width="320">
+						   <tbody>
+						   <ul>
+							<tr>
+							 <td><li>BBA (Hons.) program</li></td>
+							 <td>127 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>B.Sc (Hons.) Mathematics</li></td>
+							 <td>150 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>B.Sc (Hons.) in CSE</li></td>
+							 <td>160 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>B.Sc (Hons.) in ESC </li></td>
+							 <td>141 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>B.A (Hons.) in English</li></td>
+							 <td>123 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>LL.B (Hons.)</li></td>
+							 <td>140 credits</td>
+						   </tr>
+						   <tr>
+							 <td><li>BSS in Applied Sociology &amp; Social Works</li></td>
+							 <td>134 credits</td>
+						   </tr>
+						   </ul>
+						 </tbody>
+					   </table>
+					</div>
+					
+						<div style="height:auto;min-width:320px;">
+							<h1 style="font-size:2vw">Graduate Program:</h1>
+							<table style="height: 217px;" width="320">
+								<tbody>
+								<ul>
+								  <tr>
+									<td><li>MBA (Regular)</li></td>
+									<td>64 credits</td>
+								  </tr>
+								  <tr>
+									<td><li>MBA (Executive)</li></td>
+									<td>52 credits</td>
+								  </tr>
+								  <tr>
+									<td><li>MA in English (Preliminary)</li></td>
+									<td>36 credits (Non Degree)</td>
+								  </tr>
+								  <tr>
+									<td><li>MA in English (Final)</li></td>
+									<td>36 credits</td>
+								  </tr>
+								  <tr>
+									<td><li>LL.M (Final)</li></td>
+									<td>36 credits</td>
+								  </tr>
+								  <tr>
+									<td><li>MSc. in Mathematics</li></td>
+									<td>40 Credits</td>
+								  </tr>
+								  <tr>
+									<td><li>MSc.(Preliminary) in Mathematics</li></td>
+									<td>37 Credits (Non Degree)</td>
+								  </tr>
+								  <tr>
+									<td><li>Master of Social Work (MSW)</li></td>
+									<td>44 credits*</td>
+								  </tr>
+								  <tr>
+									<td><li>MSS in Applied Sociology</li></td>
+									<td>44 credits</td>
+								  </tr>
+								  <tr>
+									<td><li>Master in Development Studies</li></td>
+									<td>44 credits</td>
+								  </tr>
+								  </ul>
+								</tbody>
+							 </table>
+						</div>
+					</div>				
+				</div>
+				
+				<!-- <div style="width:3%; height:1415px;background:white;color:white;float:left"> -->
+				
+				<!-- </div> -->
+				
+				<div class="first_feed_right">
+				
+					<div class="first_feed_right_search">
+				
+						<input type="text" placeholder="search for" name="" style="border-color:gray;border-bottom:1px solid white ;background:#98A7A0;outline:none;width:99%;height:40%">
+						
+						<hr>
+						
+						<div  class="first_feed_right_search1">
+							<h4>Search</h4>
+						</div>
+						
+					</div>
+					
+					<div class="first_feed_right1">
+					
+					</div>
+					
+					<div class="first_feed_right2">
+					
+					</div>
+					
+					<div class="first_feed_right3">
+						<h2 style="color:#4679DF;text-align:center">Notice Board</h2>
+							<ul>
+								<li><h5 >13 Oct Regarding Make-up exam of Mid Term of Spring-2019</h5></li>
+								<li><h5 >NEUB will remain closed from 06 October 2019 to 08 October 2019</h5></li>
+								<li><h5 >Notice of Holy Buddha Purnima(20/07/2019)</h5></li>
+								<li><h5 >Other</h5></li>
+							</ul>
+					</div>
+					
+					<div class="first_feed_right4">
+					
+					</div>
+					
+					<div class="first_feed_right5">
+					
+					</div>
+					
+					<div class="first_feed_right_diff">
+				
+						<div  class="first_feed_right_diff_BA">
+							<h4 >Department of Business Administration</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_ES">
+							<h4 >Department of Environmental Science</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_CES">
+							<h4 >Department of Computer Science & Engineering</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_LJ">
+							<h4 >Department of Law And Justice</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_E">
+							<h4 >Department of English</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_PH">
+							<h4 >Department of Public Health</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_SW">
+							<h4 >Department of Applied Sociology and Social Work</h4>
+						</div>			
+					</div>
+					
+					<div class="first_feed_right6">
+					
+					</div>
+					
+					<div class="first_feed_right7">
+					
+					</div>
+					
+					<div class="first_feed_right_FB">
+						<h3 > <a href="https://www.facebook.com/NorthEastUniversityBangladesh/"><img src="fb.jpg" width="100%" height="265px"></a></h3>					
+						
+					</div>
+					
+					<div class="first_feed_right8">
+					
+					</div>
+					
+				</div>
+				
+				
+				<div style="width:100%; height:30px;background:white;color:white;float:left">
+					
+				</div>
+					
+				<div class="first_location">
+						<div class="first_location1">
+							
+						</div>
+						
+						<div class="first_location2">
+							<h2 style="color:#95702D">Campus Location</h2>
+						</div>
+						
+						<div class="first_location3">
+							
+						</div>
+						
+						<div class="first_location4">
+							<a href="https://www.google.com/maps/place/North+East+University+Bangladesh/@24.8906513,91.861097,16z/data=!4m5!3m4!1s0x3751aacd70cd7665:0xc8ae330ad72490dd!8m2!3d24.8900955!4d91.8609555"><img src="map.png" width="100%" height="400px"></a>
+						</div>
+						
+						<div class="first_location5">
+							
+						</div>
+				</div>
+					
+				<div class="first_footer1">
+					
+				</div>
+					
+				<div class="first_footer2">
+					<h3 style="color:#c3d136;text-align:left;padding-left:50px;">
+						Contact :
+					</h3>
+					<h5 style="display:block;padding-left:50px;">
+						Phone: 01718389885
+						<br>
+						Mail : gkpalash101@gmail.com
+					</h5>
+				</div>
+				<div class="first_footer3">
+			<h5 style="text-align:center;">		
+					Copyright © 2019 North East University Bangladesh, all rights reserved. Developed by Kibria.<br>
+					This site is maintained by CSE & IT Department
+			</h5>
+				</div>
+				
+		</div>
+		<script>
+			window.onscroll = function() {scrollFunction()};
+
+				function scrollFunction() {
+				  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					document.getElementById("navbar").style.top = "0";
+					 document.getElementById("navbar").style.position = 'fixed';
+					  document.getElementById("navbar").style.width = '100%';
+				  } else {
+					document.getElementById("navbar").style.top = "0px";
+					document.getElementById("navbar").style.position = 'relative';
+				  }
+				}
+		</script>
+	</body>
+</html>
---
+++ Student Portal.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Course Registration
+		</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	
+	<body>
+		<div class="full_height" >
+				
+				<div class="head">
+				
+					<div class="head1">
+
+						<div class="head1_pic">
+							<img src="logo.jpg" width="98px" height="78px">
+						</div>
+						<div class="head1_ex1">
+						</div>
+						<div class="head1_name">
+							<h3 style="text-align:left">North East University Bangladesh</h3>
+						</div>
+						<div class="head1_sign_log">
+								<!-- <div style="width:100%;height:18px;float:left;background:#0D3336">
+									
+								</div> -->
+								<div class="head1_signUp">
+									<h4 ><a href="reg.php" style="text-decoration:none">Sign Up</a></h4>
+								</div>
+								<div class="head1_signIn">
+									<h4 ><a href="Login.php">Sign In</a></h4>
+								</div>
+								<div class="head1_sign_log_ex1">
+									
+								</div>								
+								
+								<!-- <div style="width:100%;height:20px;float:left;background:#0D3336">
+	
+								</div> -->
+						</div>
+					</div>
+					
+					<hr style="width:100%; height:1px;float:left;margin:0%;background:#162199">
+					
+					<div class="head2" id="navbar">
+					
+						<!-- <div style="width:10%; height:65px;border:0px solid white;float:left;background:#0D3336;">
+						
+						</div> -->
+						<div class="head2_manu1">
+						<h4 ><a href="home.php">Home</a></h4>
+						</div>
+						<div class="head2_manu1">
+						<h4 ><a href="Academic Programs.php">About Us</a></h4>
+						</div>
+						<div class="head2_manu1">
+							<h4 ><a href="tution_fee.php">Admission</a></h4>
+						</div>
+						<div class="head2_manu2">
+							<h4 >Faculty Members</h4>
+						</div>
+						<div class="head2_manu2">
+							<h4 ><a href="Student Portal.php" style="text-decoration:none">Student Portal</a></h4>
+						</div>
+						<div class="head2_manu1">
+							<h4 >Office</h4>
+						</div>
+						<div  class="head2_manu2">
+							<h4 >Others</h4>
+						</div>
+						<div  style="width:15.35%;float:left;	background:#0D3336;height:65px;min-width:123px">
+							<h4 ></h4>
+						</div>
+					</div>
+				</div>
+				
+				<hr style="width:100%; height:2px;float:left;margin:0%;background:#162199">
+				
+				<div style="width:70%; height:auto;min-width:320px;color:white;float:left">
+					<div style="width:1%;height:700px;background:white;float:left">
+							
+					</div>
+					<div style="width:99%; height:auto;min-width:320px;background:#447499;float:left">
+						
+						<h2 >Course Registration</h2>
+						<h4 > Student Id : </h4>
+						<input type="passward" placeholder="Student Id" name="" style="border-color:gray;border-bottom:1px solid white ;background:#C7D6D6;outline:none;width:99%;height:10%">
+
+						<h4 > Date Of Birth : </h4>
+						<input type="date" placeholder="date of birth" name="" style="border-color:gray;border-bottom:1px solid white ;background:#C7D6D6;outline:none;width:99%;height:10%">
+						
+						<input type="submit" value="NEXT" style="border-color:gray;border-bottom:1px solid white ;background:#43D4CD;outline:none;width:15%;height:8% ;margin:2% 0% 0% 0.1%;min-width:110px;">
+						<hr>
+						<hr>
+			
+					</div>				
+				</div>
+				
+				<!-- <div style="width:3%; height:1415px;background:white;color:white;float:left"> -->
+				
+				<!-- </div> -->
+				
+				<div class="first_feed_right">
+				
+					<div class="first_feed_right_search">
+				
+						<input type="text" placeholder="search for" name="" style="border-color:gray;border-bottom:1px solid white ;background:#98A7A0;outline:none;width:99%;height:40%">
+						
+						<hr>
+						
+						<div  class="first_feed_right_search1">
+							<h4>Search</h4>
+						</div>
+						
+					</div>
+					
+					<div class="first_feed_right1">
+					
+					</div>
+					
+					<div class="first_feed_right2">
+					
+					</div>
+					
+					<div class="first_feed_right3">
+						<h2 style="color:#4679DF;text-align:center">Notice Board</h2>
+							<ul>
+								<li><h5 >13 Oct Regarding Make-up exam of Mid Term of Spring-2019</h5></li>
+								<li><h5 >NEUB will remain closed from 06 October 2019 to 08 October 2019</h5></li>
+								<li><h5 >Notice of Holy Buddha Purnima(20/07/2019)</h5></li>
+								<li><h5 >Other</h5></li>
+							</ul>
+					</div>
+					
+					<div class="first_feed_right4">
+					
+					</div>
+					
+					<div class="first_feed_right5">
+					
+					</div>
+					
+					<div class="first_feed_right_diff">
+				
+						<div  class="first_feed_right_diff_BA">
+							<h4 >Department of Business Administration</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_ES">
+							<h4 >Department of Environmental Science</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_CES">
+							<h4 >Department of Computer Science & Engineering</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_LJ">
+							<h4 >Department of Law And Justice</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_E">
+							<h4 >Department of English</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_PH">
+							<h4 >Department of Public Health</h4>
+						</div>
+						
+						<div  class="first_feed_right_diff_SW">
+							<h4 >Department of Applied Sociology and Social Work</h4>
+						</div>			
+					</div>
+					
+					<div class="first_feed_right6">
+					
+					</div>
+					
+					<div class="first_feed_right7">
+					
+					</div>
+					
+					<div class="first_feed_right_FB">
+						<h3 > <a href="https://www.facebook.com/NorthEastUniversityBangladesh/"><img src="fb.jpg" width="100%" height="265px"></a></h3>						
+						
+					</div>
+					
+					<div class="first_feed_right8">
+					
+					</div>
+					
+				</div>
+				
+				
+				<div style="width:100%; height:30px;background:white;color:white;float:left">
+					
+				</div>
+					
+				<div class="first_location">
+						<div class="first_location1">
+							
+						</div>
+						
+						<div class="first_location2">
+							<h2 style="color:#95702D">Campus Location</h2>
+						</div>
+						
+						<div class="first_location3">
+							
+						</div>
+						
+						<div class="first_location4">
+							<a href="https://www.google.com/maps/place/North+East+University+Bangladesh/@24.8906513,91.861097,16z/data=!4m5!3m4!1s0x3751aacd70cd7665:0xc8ae330ad72490dd!8m2!3d24.8900955!4d91.8609555"><img src="map.png" width="100%" height="400px"></a>
+						</div>
+						
+						<div class="first_location5">
+							
+						</div>
+				</div>
+					
+				<div class="first_footer1">
+					
+				</div>
+					
+				<div class="first_footer2">
+					<h3 style="color:#c3d136;text-align:left;padding-left:50px;">
+						Contact :
+					</h3>
+					<h5 style="display:block;padding-left:50px;">
+						Phone: 01718389885
+						<br>
+						Mail : gkpalash101@gmail.com
+					</h5>
+				</div>
+				<div class="first_footer3">
+			<h5 style="text-align:center;">		
+					Copyright © 2019 North East University Bangladesh, all rights reserved. Developed by Kibria.<br>
+					This site is maintained by CSE & IT Department
+			</h5>
+				</div>
+				
+		</div>
+		
+		<script>
+			window.onscroll = function() {scrollFunction()};
+
+				function scrollFunction() {
+				  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					document.getElementById("navbar").style.top = "0";
+					 document.getElementById("navbar").style.position = 'fixed';
+					  document.getElementById("navbar").style.width = '100%';
+				  } else {
+					document.getElementById("navbar").style.top = "0px";
+					document.getElementById("navbar").style.position = 'relative';
+				  }
+				}
+		</script>
+	</body>
+</html>
